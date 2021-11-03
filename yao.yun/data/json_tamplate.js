a7ba20891affcad0705df33d4545fa81a607e467
Yun Yao <yaoyun0831@gmail.com>
2021-11-02 21:14:33 -0700

    json files

--- yao.yun/data/json_tamplate.js
+++ yao.yun/data/json_tamplate.js
@@ -1,5 +1,82 @@
 // https://www.json-generator.com/
 
+// USERS DATA
+[
+  '{{repeat(10)}}',
+  {
+    id: '{{index(1)}}',
+    name: '{{firstName()}} {{surname()}}',
+    username: function() {
+      return 'user' + this.id;
+    },
+    email: function() {
+      return this.username + '@gmail.com';
+    },
+    password: 'md5(pass)',
+    img: function(tags) {
+      return 'https://via.placeholder.com/400/'+
+        tags.integer(700,999) + '/fff/?text=' + this.username;
+    },
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+] 
+
+
+
+
+// ANIMALS DATA
+[
+  '{{repeat(50)}}',
+  {
+    id: '{{index(1)}}',
+    user_id: '{{integer(1,10)}}',
+    
+    name: '{{company()}}',
+    
+    type: '{{random("horse","dog","cat")}}',
+    breed: function(tags) {
+      var breeds = {
+        dog:["labrador","pitbull","dachsund"],
+        cat:["calico","ginger","tuxedo","siamese"],
+        horse:["shetland","andalusian","unicorn"]
+      };
+      var chosen_type = breeds[this.type];
+      var chosen_index = tags.integer(0,chosen_type.length-1);
+      return chosen_type[chosen_index];
+    },
+    
+    description: '{{lorem(3,"sentences")}}',
+    
+    img: function(tags) {
+      return 'https://via.placeholder.com/400/'+
+        tags.integer(700,999) + '/fff/?text=' + this.name;
+    },
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+] 
+
+
+// LOCATIONS DATA
+[
+  '{{repeat(250)}}',
+  {
+    id: '{{index(1)}}',
+    animal_id: '{{integer(1,50)}}',
+    lat: '{{floating(37.801515, 37.699475)}}',
+    lng: '{{floating(-122.506533, -122.388415)}}',
+    
+    description: '{{lorem(3,"sentences")}}',
+    
+    photo: 'https://via.placeholder.com/400/',
+    icon: 'https://via.placeholder.com/400/?text=ICON',
+    date_create: '{{date(new Date(2020, 0, 1), new Date(), "YYYY-MM-dd hh:mm:ss")}}'
+  }
+] 
+
+
+
+
+// ORIGINAL
 [
   '{{repeat(5, 7)}}',
   {
@@ -40,4 +117,4 @@
       return fruits[tags.integer(0, fruits.length - 1)];
     }
   }
-]
+] 
